b775a0b32c564f3509e116365ffde0c5dadd24bc
Adrian-Jk <adrianjuraszek88@gmail.com>
2026-04-17 13:47:52 +0200

    Update SignUpPage.js

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,4 +1,4 @@
-import { expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 export class SignUpPage {
   constructor(page) {
@@ -11,23 +11,33 @@ export class SignUpPage {
   }
 
   async open() {
+    await test.step(`Open 'Sign Up' page`, async () => {
     await this.page.goto('/user/register');
+    });
   }
 
   async fillUsernameField(username) {
+    await test.step(`Fill 'User name' field`, async () => {
     await this.usernameField.fill(username);
+    });
   }
 
   async fillEmailField(email) {
+    await test.step(`Fill 'Email' field`, async () => {
     await this.emailField.fill(email);
+    });
   }
 
   async fillPasswordField(password) {
+    await test.step(`Fill 'Password' field`, async () => {
     await this.passwordField.fill(password);
+    });
   }
 
   async clickSignUpButton() {
+    await test.step(`Click 'Sign Up' button`, async () => {
     await this.signUpButton.click();
+    });
   }
 
   async assertErrorMessageContainsText(messageText) {
