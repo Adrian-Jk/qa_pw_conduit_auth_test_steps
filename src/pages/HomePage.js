919f77af95592f5ea436def3d617ebc637cc0cd0
Adrian-Jk <adrianjuraszek88@gmail.com>
2026-04-17 14:46:53 +0200

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,11 +3,13 @@ import { test,expect } from '@playwright/test';
 export class HomePage {
   constructor(page) {
     this.page = page;
-    this.yourFeedTab = page.getByText('Your Feed');
+    this.yourFeedTab = page.getByRole('link', { name: 'Your Feed' });
   }
 
   async assertYourFeedTabIsVisible() {
     await test.step("Assert 'Your Feed' tab is visible", async () => {
+    await expect(this.page).toHaveURL(/.*/);
     await expect(this.yourFeedTab).toBeVisible();
   });
+ }
 }
